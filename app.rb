970cdecb49609f6738af8692d082b381f9ef6e8f
James Hart <jhart@rpxcorp.com>
2012-10-29 22:30:55 -0700

    Added caption support and also some prowly start

--- app.rb
+++ app.rb
@@ -1,71 +1,78 @@
 require 'rubygems'
-require 'next_muni'
 require 'sinatra'
-require 'yaml'
 require 'awesome_print'
+require 'sinatra/flash'
+require 'sinatra/redirect_with_flash'
+require 'prowly'
 
-get '/buses' do
-  @routes = NextMuni.get_routes('sf-muni')
-  erb :buses
-end
-
-post '/add' do
-  pages = YAML.load(File.open('buses.yml', 'r').read)
-  params[:bus]
-  params[:bus_stop]
-  params[:page]
-  
-  "#{params[:page]} on bus #{params[:bus]} with stop #{params[:bus_stop]}"
-
-  page = pages.select { |page| page[:label] == params[:page] }
-  return "Error saving the page" if page.empty?
-  
-  buses = page.first[:buses]
-  buses << {
-    :label => params[:label],
-    :stop => params[:bus_stop],
-    :bus => params[:bus]
-  }
-  
-  File.open('buses.yml', 'w') { |f| f.puts pages.to_yaml }
-  redirect "/#{params[:page]}"
-end
-
-get '/bus/:bus_no' do
-  @bus = params[:bus_no]
-  @directions = NextMuni.get_stops(@bus)
-  erb :add
-end
-
-get '/bus/:bus_no/:bus_stop' do
-  @bus = params[:bus_no]
-  @bus_stop = params[:bus_stop]
-  @times = NextMuni.get_times(@bus, @bus_stop)
-  pages = YAML.load(File.open('buses.yml', 'r').read)
-  @links = pages.map { |page| page[:label] }
-  erb :times
-end
-
-get '/:name' do
-  pages = YAML.load(File.open('buses.yml', 'r').read)  
-  page = pages.select { |page| page[:label] == params[:name] }
-  if page.empty?
-    "Page not found" 
-  else
-    page = page.first
-    @stops = page[:buses].map do |bus|
-      {
-      :bus => bus[:label],
-      :times => NextMuni.get_times(bus[:bus].to_s, bus[:stop])
-      }
-    end
-    @links = pages.map { |page| page[:label] }
-    erb :stops
-  end
-end
+enable :sessions
 
 get '/' do
-  pages = YAML.load(File.open('buses.yml', 'r').read)  
-  redirect "/#{pages.first[:label]}"
+  emoji_hash = {
+        'pile_of_poo' => '&#128169;',
+        'fist_bump: ' => '&#x1F44A;',
+        'head orgasm' => '&#x1F486;',
+        'scary man' => '&#x1F479;',
+        'even scarrier man' => '&#x1F47A;',
+        'flying money' => '&#x1F4B8;',
+        'grimace' => '&#x1F629;',
+        'what' => '&#x1F640;',
+        'monkey ass scoot' => '&#x1F412;',
+        'pile o poo' => '&#x1F4A9;',
+        'dirty dirty fucking mustache man' => '&#x1F468;',
+        'lightning bolt' => '&#x26A1;', 
+        'snowman' => '&#x26C4;',
+        'sparkle motion' => '&#x2728;',
+        'cityscape' => '&#x1F307;',
+        'sunflower' => '&#x1F33B;',
+        'cheeseburger' => '&#x1F354;',
+        'emojifry' => '&#x1F35F;',
+        'egg' => '&#x1F373;',
+        'beer' => '&#x1F37B;',
+        'uhhh' => '&#x1F390;',
+        'rollercoaster' => '&#x1F3A2;',
+        'ram' => '&#x1F411;',
+        'warthog' => '&#x1F417;',
+        'caterpillar' => '&#x1F41B;',
+        'koala' => '&#x1F428;',
+        'camel' => '&#x1F42B;',
+        'dolphin' => '&#x1F42C;',
+        'mouse' => '&#x1F42D;',
+        'cow' => '&#x1F42E;',
+        'idiot' => '&#x1F47C;',
+        'blood needle' => '&#x1F489;',
+        'lovahs ' => '&#x1F48F;',
+        'bob-omb' => '&#x1F4A3;',
+        'laserdisc' => '&#x1F4BD;',
+        'iphone' => '&#x1F4F1;',
+        'cellphones off' => '&#x1F4F4;',
+        'nobody over 18' => '&#x1F51E;',
+        'flame' => '&#x1F525;',
+        'slap hands' => '&#x1F64F;',
+        'pepsi sign' => '&#x1F68F;',
+        'cock blocker' => '&#x1F645;',
+        'cock blockee' => '&#x1F6B6;',
+        'bad ass boat' => '&#x1F6A4;',
+        'rockets' => '&#x1F680;',
+        'pig snout' => '&#x1F43D;',
+      }
+  values = emoji_hash.values
+  keys = emoji_hash.keys
+  
+  random_seed = rand(values.size)
+  @emoji = values[random_seed]
+  @emoji_id = keys[random_seed]
+  
+  erb :default
+end
+
+post '/' do
+  caption = params[:caption]
+  caption_id = params[:caption_id]
+  File.open('captions.txt', 'a+') { |f|
+    f.puts "#{caption_id}, #{caption}"
+  }
+  
+  redirect '/', :notice => "Duly noted!"
 end
 
--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,34 +1,31 @@
-;(function ($, window, undefined) {
+;
+(function ($, window, undefined) {
   'use strict';
 
   var $doc = $(document),
-      Modernizr = window.Modernizr;
+    Modernizr = window.Modernizr;
 
-  
-  $.fn.foundationAlerts           ? $doc.foundationAlerts() : null;
-  $.fn.foundationAccordion        ? $doc.foundationAccordion() : null;
-  $.fn.foundationTooltips         ? $doc.foundationTooltips() : null;
+
+  $.fn.foundationAlerts ? $doc.foundationAlerts() : null;
+  $.fn.foundationAccordion ? $doc.foundationAccordion() : null;
+  $.fn.foundationTooltips ? $doc.foundationTooltips() : null;
   $('input, textarea').placeholder();
-  
-  
-  $.fn.foundationButtons          ? $doc.foundationButtons() : null;
-  
-  
-  $.fn.foundationNavigation       ? $doc.foundationNavigation() : null;
-  
-  
-  $.fn.foundationTopBar           ? $doc.foundationTopBar() : null;
-  
-  $.fn.foundationCustomForms      ? $doc.foundationCustomForms() : null;
+
+
+  $.fn.foundationButtons ? $doc.foundationButtons() : null;
+
+
+  $.fn.foundationNavigation ? $doc.foundationNavigation() : null;
+
+
+  $.fn.foundationTopBar ? $doc.foundationTopBar() : null;
+
+  $.fn.foundationCustomForms ? $doc.foundationCustomForms() : null;
   $.fn.foundationMediaQueryViewer ? $doc.foundationMediaQueryViewer() : null;
-  
-    
-    $.fn.foundationTabs             ? $doc.foundationTabs() : null;
-    
-  
-  
-    $("#featured").orbit();
-  
+
+
+  $.fn.foundationTabs ? $doc.foundationTabs() : null;
+
 
   // UNCOMMENT THE LINE YOU WANT BELOW IF YOU WANT IE8 SUPPORT AND ARE USING .block-grids
   // $('.block-grid.two-up>li:nth-child(2n+1)').css({clear: 'both'});
@@ -44,5 +41,32 @@
       }, 0);
     });
   }
+  
+  	$(document).ready(function() {
+  		var timeout = setTimeout(function() {
+  			$('.alert-box').fadeOut();
+  		}, 3000);
+  		
+  		$('#caption').focus();
+      
+  	});
+  
 
 })(jQuery, this);
+
+$(document).ready(function () {
+  var ua = navigator.userAgent.toLowerCase();
+  var using_safari = false;
+  if (ua.indexOf('safari') != -1) {
+    if (ua.indexOf('chrome') > -1) {
+    } else {
+      using_safari = true;
+    }
+  }
+
+  if(!using_safari) {
+    console.log("no safari!");
+    var html = '<div class="panel"><h3>You are not seeing the right thing!</h3><h5 class="subheader">This website really makes no sense unless you are using Safari. If it is not already installed on your computer, it probably is not worth your time to go and get it, but if you insist – you can do so below</h5><a class="button" href="http://www.apple.com/safari/download">Download Safari »</a></div>'
+    $('#container').append(html);
+  }
+});
